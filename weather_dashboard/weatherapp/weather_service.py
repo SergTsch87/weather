e7286efae0ef12243cf2141290f7eb49609d5519
SergTsch87 <sergtsch87@gmail.com>
2026-03-08 09:58:03 +0200

    Add wapp/weather_service.py

---
+++ weather_dashboard/weatherapp/weather_service.py
@@ -0,0 +1,15 @@
+import requests
+import os
+from django.conf import settings
+
+
+def weather_data(city):
+    api_key = os.getenv('OPENWEATHERMAP_API_KEY')
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+
+    try:
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        print(f'Error fetching weather data: {e}')
+        return None
