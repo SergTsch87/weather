f57fe8ced00bcf9e92ec8651e50b097153e8003d
SergTsch87 <sergtsch87@gmail.com>
2026-03-09 15:12:34 +0200

    Install dotenv + Upd w_app/ weather.html + w_service.py

--- weather_dashboard/weatherapp/weather_service.py
+++ weather_dashboard/weatherapp/weather_service.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
 import requests
 import os
 
 
 def get_weather_data(city):
+    load_dotenv()
     api_key = os.getenv('OPENWEATHERMAP_API_KEY')
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+    # url = f"https://api.openweathermap.org/data/3.0/weather?q={city}&units=metric&appid={api_key}"
 
     try:
         response = requests.get(url)
